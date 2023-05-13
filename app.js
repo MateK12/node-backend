814571577b3d2bf60887d9b55e5f37392fd5b529
kristichmateo <kristichmateo@gmail.com>
2023-05-12 23:19:32 -0300

    conexion y CRUD mongo

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { MongoClient } = require('mongodb');
+const {ObjectId} = require('mongodb')
+const mongoose = require('mongoose')
+const MONGO_HOST="localhost"
+const MONGO_PUERTO="27017"
+const MONGO_URI="mongodb://"+MONGO_HOST+":"+MONGO_PUERTO+"/"
+
+// mongoose.connect(MONGO_URI, { useUnifiedTopology: true })       //promesa para conectar a compass
+//     .then(() => { console.log('Connected to MongoDB: %s \n ', MONGO_URI) }) //si sale bien
+//     .catch((err) => { console.log('MongoDB connection error: %s \n', err); }) //si sale mal
+
+const client = new MongoClient(MONGO_URI) //me conecto con URI
+const dbname = "prueba"
+const coll = "Coleccion"
+let coleccion = client.db(dbname).collection(coll) //creo el db.coll
+let objeto = {
+    "titulo":"el principito",
+    "descripcion":"elefante en un gorro"
+}
+
+async function main(){
+    let insertar = await  coleccion.insertOne(objeto) //inserto un documento
+    console.log(insertar)
+    let leer = await coleccion.findOne({"_id": new ObjectId("6459747ad1f78d866b617708")})
+    console.log(leer)
+    coleccion.close
+}
+main()
+
+client.close //cierro conexion
 
 app.use(
     express.urlencoded({
